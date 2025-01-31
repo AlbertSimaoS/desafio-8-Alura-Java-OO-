684436be40879cc7eb700ef70ee355560ad1e678
Albert <72774907+AlbertSimaoS@users.noreply.github.com>
2025-01-31 15:31:38 -0300

    Update VerificarPrimo.java

--- src/VerificarPrimo.java
+++ src/VerificarPrimo.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
-public class VerificarPrimo {
+public class VerificarPrimo extends NumerosPrimos{
+    public void verificarSeEhPrimo(int numero) {
+        if (verificarPrimalidade(numero)) {
+            System.out.println(numero + " é primo.");
+        } else {
+            System.out.println(numero + " não é primo.");
+        }
+    }
 }
